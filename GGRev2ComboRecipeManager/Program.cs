db755a288c5bd9853a4d394879b1aa0c01929731
Wessel Kuipers <wesselkuipers@gmail.com>
2017-07-21 01:13:50 +0200

    Add project files.

---
+++ GGRev2ComboRecipeManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GGRev2ComboRecipeManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
