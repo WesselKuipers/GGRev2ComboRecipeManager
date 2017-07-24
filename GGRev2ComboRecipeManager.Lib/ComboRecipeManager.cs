c0f2fb4c513ee3192a71f236e97cf5f71c131d2d
Wessel Kuipers <wesselkuipers@gmail.com>
2017-07-24 21:39:03 +0200

    Fetch offset for slot1 based on pointer

--- GGRev2ComboRecipeManager.Lib/ComboRecipeManager.cs
+++ GGRev2ComboRecipeManager.Lib/ComboRecipeManager.cs
@@ -10,7 +10,7 @@ namespace GGRev2ComboRecipeManager.Lib
         const int PROCESS_VM_WRITE = 0x0020;
         const int PROCESS_VM_OPERATION = 0x0008;
 
-        const int SLOT1_OFFSET = 0x1AD8194;
+        const int SLOT1_OFFSET = 0x00BD40A4;
         public const int SLOT_DATA_SIZE = 1028;
         public const int SLOT_CHARCODE_SIZE = 4;
 
@@ -35,15 +35,15 @@ namespace GGRev2ComboRecipeManager.Lib
             var processHandle = OpenProcess(PROCESS_WM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, process.Id);
 
             var bytesRead = 0;
-            var buffer = new byte[SLOT_DATA_SIZE * 5];
+            var comboRecipeData = new byte[SLOT_DATA_SIZE * 5];
 
-            ReadProcessMemory((int)processHandle, SLOT1_OFFSET, buffer, buffer.Length, ref bytesRead);
+            ReadProcessMemory((int)processHandle, GetSlot1Offset(process), comboRecipeData, comboRecipeData.Length, ref bytesRead);
 
-            var combo1 = new ComboRecipe(buffer, 0);
-            var combo2 = new ComboRecipe(buffer, 1);
-            var combo3 = new ComboRecipe(buffer, 2);
-            var combo4 = new ComboRecipe(buffer, 3);
-            var combo5 = new ComboRecipe(buffer, 4);
+            var combo1 = new ComboRecipe(comboRecipeData, 0);
+            var combo2 = new ComboRecipe(comboRecipeData, 1);
+            var combo3 = new ComboRecipe(comboRecipeData, 2);
+            var combo4 = new ComboRecipe(comboRecipeData, 3);
+            var combo5 = new ComboRecipe(comboRecipeData, 4);
 
             return new[] {combo1, combo2, combo3, combo4, combo5};
         }
@@ -56,12 +56,12 @@ namespace GGRev2ComboRecipeManager.Lib
                 return null;
             }
 
-            var processHandle = OpenProcess(PROCESS_WM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, false, process.Id);
+            var processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
 
             var bytesRead = 0;
             var buffer = new byte[SLOT_DATA_SIZE];
 
-            ReadProcessMemory((int)processHandle, process.MainModule.BaseAddress.ToInt32() + SLOT1_OFFSET * slotNr, buffer, buffer.Length, ref bytesRead);
+            ReadProcessMemory((int)processHandle, GetSlot1Offset(process) + SLOT1_OFFSET * slotNr, buffer, buffer.Length, ref bytesRead);
 
             return new ComboRecipe(buffer, slotNr);
         }
@@ -79,7 +79,20 @@ namespace GGRev2ComboRecipeManager.Lib
             var data = recipe.ToRecipeData();
             var bytesWritten = 0;
 
-            WriteProcessMemory((int)processHandle, process.MainModule.BaseAddress.ToInt32() + SLOT1_OFFSET + (SLOT_DATA_SIZE * slotNr), data, data.Length, ref bytesWritten);
+            WriteProcessMemory((int)processHandle, GetSlot1Offset(process) + (SLOT_DATA_SIZE * slotNr), data, data.Length, ref bytesWritten);
+        }
+
+        private static int GetSlot1Offset(Process process)
+        {
+            var processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+
+            var bytesRead = 0;
+            var bufferAddress = new byte[4];
+
+            var ptr = IntPtr.Add(process.MainModule.BaseAddress, SLOT1_OFFSET);
+            ReadProcessMemory((int)processHandle, (int)ptr, bufferAddress, 4, ref bytesRead);
+
+            return BitConverter.ToInt32(bufferAddress, 0);
         }
     }
 }
